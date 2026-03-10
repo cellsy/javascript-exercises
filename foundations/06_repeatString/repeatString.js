666462f10f8be06b07103f572ddc82bd9d67059b
cellsy <carson.ellsworth@gmail.com>
2026-03-10 14:07:55 -0700

    solved the first ten problems

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
 const repeatString = function() {
 
+    let numRepeats = arguments[1]
+    if (numRepeats < 0) return "ERROR"
+    let stringToRepeat = arguments[0]
+    let repeatedString = ""
+    console.log(numRepeats, stringToRepeat)
+    for (let i = 0; i < numRepeats; i++) {
+        repeatedString += stringToRepeat
+    }
+    console.log(repeatedString)
+
+    return repeatedString
 };
 
 // Do not edit below this line
