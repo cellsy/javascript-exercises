3372e155fc75a28bc95a6f256666cca9787d2ee9
cellsy <carson.ellsworth@gmail.com>
2026-03-11 10:19:15 -0700

    practicing arrays

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+
+    //first find where the element is
+    inputArray = arguments[0]
+    let locatedAt = 0
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === arguments[1]) 
+            locatedAt = i
+    }
+    console.log(arguments[0])
+    let returnArray = arguments[1](3,1)
+    console.log(returnArray);
+    return arguments[0].splice(locatedAt,1)    
 };
 
 // Do not edit below this line
